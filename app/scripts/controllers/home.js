8a927ca9c5b3270b995cec3f6303276218451b03
konstantina-p <konstantina_p@windowslive.com>
2015-03-19 11:57:49 +0100

    search partially implemented

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -5,7 +5,25 @@
  * # ArtistsCtrl
  * Controller of the musicPlayerApp
  */
-angular.module('musicPlayerApp')
-  .controller('HomeCtrl', function ($scope) {
-  	console.log("home controller");
-  });
+  angular.module('musicPlayerApp')
+  .controller('HomeCtrl', function ($scope, Spotify) {
+  	$scope.query;
+  	$scope.artists=[];
+  	$scope.albums=[];
+  	$scope.songs=[];
+
+  	$scope.searchFor = function(){  		
+  		Spotify.search($scope.query,'artist,album,track', {limit:'5'}).then(function (data) {  		
+  		$scope.artists=[];
+  		$scope.albums=[];
+  		$scope.songs=[];
+  		$scope.artists.push(data.artists.items);
+  		$scope.albums.push(data.albums.items);
+  		$scope.songs.push(data.tracks.items); 
+  		console.log($scope.artists);
+  		console.log($scope.albums);
+  		console.log($scope.songs);
+  	});
+  	}
+	
+});
