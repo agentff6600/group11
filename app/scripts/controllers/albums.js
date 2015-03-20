e2b83edbf4fbad346e179a1c9d9a336ac049bed6
i9maki2 <Mladen@Mladens-MacBook-Pro.local>
2015-03-20 02:38:51 +0100

    albumsCtrl fixed

--- app/scripts/controllers/albums.js
+++ app/scripts/controllers/albums.js
@@ -5,24 +5,12 @@
  * # ArtistsCtrl
  * Controller of the musicPlayerApp
  */
-angular.module('musicPlayerApp')
-  .controller('AlbumsCtrl', function ($scope, Spotify) {
-  	console.log("albums controller");  	
+angular.module('musicPlayerApp') 	
   .controller('AlbumsCtrl', function ($scope, $rootScope, Spotify) {
-    console.log("albums controller");   
-
-  	$scope.getAlbumsFromArtist = function(artist){  		
+    console.log("albums controller");   		
     $scope.getAlbumsFromArtist = function(artist){      
-
-  		//Data is retrieved only from Swedesh Market (if other country required change 'SE' string)
-  		//Limited to 20 Albums (can be extended to maximum 50 - just add in {limit: 50})
       //Data is retrieved only from Swedesh Market (if other country required change 'SE' string)
       //Limited to 20 Albums (can be extended to maximum 50 - just add in {limit: 50})
-
-  		Spotify.getArtistAlbums(artist,{country:'SE'}).then(function (data) {  		
-  		$scope.albums = data.items;     		
-	});
-  	}
   	
       Spotify.getArtistAlbums(artist,{country:'SE'}).then(function (data) {     
       $scope.albums = data.items;         
